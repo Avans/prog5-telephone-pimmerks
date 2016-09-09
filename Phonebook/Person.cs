48df613192ef388f4396e1b291ef58e74e6a5e5c
ppiimm123 <pimmerks@msn.com>
2016-09-09 21:14:00 +0200

    Initial Commit

---
+++ Phonebook/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+	class Person
+	{
+		public String FirstName { get; set; }
+		public String LastName { get; set; }
+		public int PhoneNumber { get; set; }
+
+		public String FullName { get { return FirstName + " " + LastName; }}
+
+		public Person(String firstName, String lastName)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+		}
+
+
+	}
+}
